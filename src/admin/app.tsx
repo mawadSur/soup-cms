938fa426a148486e9470d5b1bd0db618ae6492d5
asta-nguyen <asta.dev.520@gmail.com>
2024-12-12 10:37:44 +0700

    setup

---
+++ src/admin/app.tsx
@@ -0,0 +1,39 @@
+import type { StrapiApp } from "@strapi/strapi/admin";
+
+export default {
+  config: {
+    tutorials: false,
+    theme: "light",
+    locales: [
+      // 'ar',
+      // 'fr',
+      // 'cs',
+      // 'de',
+      // 'dk',
+      // 'es',
+      // 'he',
+      // 'id',
+      // 'it',
+      // 'ja',
+      // 'ko',
+      // 'ms',
+      // 'nl',
+      // 'no',
+      // 'pl',
+      // 'pt-BR',
+      // 'pt',
+      // 'ru',
+      // 'sk',
+      // 'sv',
+      // 'th',
+      // 'tr',
+      // 'uk',
+      // 'vi',
+      // 'zh-Hans',
+      // 'zh',
+    ],
+  },
+  bootstrap(app: StrapiApp) {
+    console.log(app);
+  },
+};
